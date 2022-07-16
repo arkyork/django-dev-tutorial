c974302f006fff78ce1be18a129ad76207f35fc7
arkyork <abcvfga123123@gmail.com>
2022-07-16 18:16:08 +0900

    'sec'

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='first_app'
 
 urlpatterns=[
-    path('hello',views.index,name='index')
+    path('hello',views.index,name='index'),
+    path('mypage/<str:user_name>',views.mypage,name='mypage'),
+    path('sum/<int:a>/<int:b>',views.Sum,name='Sum')
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>かかかっかか</h1>')
+
+def mypage(request,user_name):
+    return HttpResponse(f'<h1>{user_name}様のページです</h1>')
+
+def Sum(request,a,b):
+    return HttpResponse(f'<h3>計算結果は{a+b}です</h3>')
